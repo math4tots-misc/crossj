179d3c70ed98cd4d3c95f6837f8eecb390d588c7
Kyumin Kim <math4tots@users.noreply.github.com>
2020-09-03 11:53:25 -0700

    Func*/Pair/Tuple* classes in crossj

--- support/shared/crossj/Tuple3.java
+++ support/shared/crossj/Tuple3.java
@@ -1,16 +1,18 @@
 package crossj;
 
-public final class Pair<A1, A2> {
+public final class Tuple3<A1, A2, A3> {
     private final A1 a1;
     private final A2 a2;
+    private final A3 a3;
 
-    private Pair(A1 a1, A2 a2) {
+    private Tuple3(A1 a1, A2 a2, A3 a3) {
         this.a1 = a1;
         this.a2 = a2;
+        this.a3 = a3;
     }
 
-    public static <A1, A2> Pair<A1, A2> of(A1 a1, A2 a2) {
-        return new Pair<>(a1, a2);
+    public static <A1, A2, A3> Tuple3<A1, A2, A3> of(A1 a1, A2 a2, A3 a3) {
+        return new Tuple3<>(a1, a2, a3);
     }
 
     public A1 get1() {
@@ -21,16 +23,20 @@ public final class Pair<A1, A2> {
         return a2;
     }
 
+    public A3 get3() {
+        return a3;
+    }
+
     public List<Object> toList() {
-        return List.of(a1, a2);
+        return List.of(a1, a2, a3);
     }
 
     @Override
     public boolean equals(Object other) {
-        if (!(other instanceof Pair<?, ?>)) {
+        if (!(other instanceof Tuple3<?, ?, ?>)) {
             return false;
         }
-        return toList().equals(((Pair<?, ?>) other).toList());
+        return toList().equals(((Tuple3<?, ?, ?>) other).toList());
     }
 
     @Override
