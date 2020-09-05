59fc27216bfb22e4638aedb287163d4d2aa5a10a
Kyumin Kim <math4tots@users.noreply.github.com>
2020-09-05 10:21:40 -0700

    Bring back crossj.Map

---
+++ support/java/crossj/Map.java
@@ -0,0 +1,40 @@
+package crossj;
+
+import java.util.HashMap;
+
+public final class Map<K, V> {
+    private final HashMap<K, V> map;
+
+    private Map(HashMap<K, V> map) {
+        this.map = map;
+    }
+
+    @SafeVarargs
+    public static <K, V> Map<K, V> of(Pair<K, V>... pairs) {
+        HashMap<K, V> map = new HashMap<>();
+        for (Pair<K, V> pair: pairs) {
+            map.put(pair.get1(), pair.get2());
+        }
+        return new Map<>(map);
+    }
+
+    public int size() {
+        return map.size();
+    }
+
+    public V get(K key) {
+        return map.get(key);
+    }
+
+    public void put(K key, V value) {
+        map.put(key, value);
+    }
+
+    public boolean containsKey(K key) {
+        return map.containsKey(key);
+    }
+
+    public XIterator<K> keys() {
+        return XIterator.fromIterator(map.keySet().iterator());
+    }
+}
