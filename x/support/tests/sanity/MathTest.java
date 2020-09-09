c790276add087ababcb70cfe349da7f6e5d33e2a
Kyumin Kim <math4tots@users.noreply.github.com>
2020-09-08 17:49:53 -0700

    Update MathTest.java

--- x/support/tests/sanity/MathTest.java
+++ x/support/tests/sanity/MathTest.java
@@ -7,4 +7,9 @@ public final class MathTest {
     public static void misc() {
         Assert.that(M.abs(M.PI - 3.141592653589793) < 0.0000001);
     }
+
+    @Test
+    public static void abs() {
+        Assert.equals(M.abs(-12), 12.0);
+    }
 }
