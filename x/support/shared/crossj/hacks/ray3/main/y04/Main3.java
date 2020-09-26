b9aab78cc9352781c9f124a537c0c3e201d3f410
Kyumin Kim <math4tots@users.noreply.github.com>
2020-09-26 15:19:12 -0700

    Update Main3.java

--- x/support/shared/crossj/hacks/ray3/main/y04/Main3.java
+++ x/support/shared/crossj/hacks/ray3/main/y04/Main3.java
@@ -6,6 +6,10 @@ import crossj.hacks.ray.Matrix;
 import crossj.hacks.ray3.RayTracer;
 import crossj.hacks.ray3.geo.Camera;
 import crossj.hacks.ray3.geo.ObjLoader;
+import crossj.hacks.ray3.geo.Sphere;
+import crossj.hacks.ray3.geo.Surfaces;
+import crossj.hacks.ray3.geo.Triangle;
+import crossj.hacks.ray3.material.Lambertian;
 import crossj.hacks.ray3.material.Metal;
 
 /**
@@ -15,7 +19,8 @@ public final class Main3 {
     private static final String filepath = "out/ray3/y04-3.bmp";
 
     public static void main(String[] args) {
-        // var objLoader = ObjLoader.usingMaterial(Metal.withColor(Color.rgb(0.9, 0.9, 0.9)));
+        // var objLoader = ObjLoader.usingMaterial(Metal.withColor(Color.rgb(0.9, 0.9,
+        // 0.9)));
         var objLoader = ObjLoader.usingMaterial(Metal.withColor(Color.rgb(0.6, 0.4, 0.4)));
         var camera = Camera.of(
                 // lookFrom
@@ -29,7 +34,19 @@ public final class Main3 {
                 // vfov (vertical field of view)
                 Camera.DEFAULT_FIELD_OF_VIEW, Camera.DEFAULT_ASPECT_RATIO);
         var tracer = RayTracer.getDefault().withCamera(camera).withVerbose(true);
-        var scene = objLoader.load("data/ray/cow.obj");
+        var cow = objLoader.load("data/ray/cow.obj");
+        IO.println("cow.getBoundingBox() = " + cow.getBoundingBox());
+        var scene = Surfaces.of(
+                // main object
+                cow,
+                // big mirrors
+                Triangle.withMaterial(Metal.withColor(Color.rgb(0.6, 0.6, 0.6))).andAt(
+                    Matrix.point(-20, 0, -12), Matrix.point(20, 0, -12), Matrix.point(0, 30, -12)),
+                Triangle.withMaterial(Metal.withColor(Color.rgb(0.6, 0.6, 0.6))).andAt(
+                    Matrix.point(-20, 0, -12), Matrix.point(-20, 0, 32), Matrix.point(-20, 30, 0)),
+                // ground
+                Sphere.withMaterial(Lambertian.withColor(Color.rgb(0.8, 0.8, 0.4)))
+                        .andTransform(Matrix.scaling(1000, 1000, 1000).thenTranslate(0, -1005, -1)));
 
         IO.println("Starting render");
         tracer.renderToBitmapFile(scene, filepath);
