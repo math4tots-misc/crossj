f2a21b0276740553ca95606963c061d086f50ea9
Kyumin Kim <math4tots@users.noreply.github.com>
2020-12-10 22:57:10 -0800

    CJ: slightly smarter auto-complete and autoimport

--- ide/language-cj/src/extension.ts
+++ ide/language-cj/src/extension.ts
@@ -1,16 +1,90 @@
+import { count } from 'console';
 import * as vscode from 'vscode';
 import * as model from './model';
+
+
+const COMMAND_AUTO_IMPORT = 'language-cj.autoimport';
 
 
 export function activate(context: vscode.ExtensionContext) {
 
     const fs = vscode.workspace.fs;
+    const world = new model.World(fs);
+
+    async function addSourceRoot(uri: vscode.Uri) {
+        const triple = model.parseSourceUri(uri);
+        if (triple !== null) {
+            const [srcroot,,] = triple;
+            await world.addSourceRoot(srcroot);
+        }
+    }
+
+    function lazyInit(documentUri: vscode.Uri) {
+        addSourceRoot(documentUri);
+        for (const editor of vscode.window.visibleTextEditors) {
+            addSourceRoot(editor.document.uri);
+        }
+    }
+
+    context.subscriptions.push(vscode.commands.registerCommand(COMMAND_AUTO_IMPORT, (qualifiedName: string) => {
+        if (model.IMPORT_EXEMPT_CLASSES.has(qualifiedName)) {
+            return;
+        }
+        const document = vscode.window.activeTextEditor?.document;
+        if (document === undefined) {
+            return;
+        }
+        {
+            const triple = model.parseSourceUri(document.uri);
+            if (triple !== null) {
+                const [, pkg, clsname] = triple;
+                if (qualifiedName === `${pkg}.${clsname}`) {
+                    // You don't need to import the item in its own file.
+                    return;
+                }
+            }
+        }
+        let pkgline = 0;
+        let insertLineno = 0;
+        let line = document.lineAt(insertLineno).text;
+        while (line === '' || line.startsWith('#') || line.startsWith('package ')) {
+            if (line.startsWith('package ')) {
+                pkgline = insertLineno;
+            }
+            insertLineno++;
+            line = document.lineAt(insertLineno).text.trim();
+        }
+        const addExtraNewline = !line.startsWith('import ');
+
+        for (let ln = insertLineno; line.startsWith('import '); ln++, line = document.lineAt(ln).text) {
+            const match = /import\s+([\w\.]+)/.exec(line);
+            console.log(`MATCH = ${match !== null ? match[1] : 'nomatch'}`);
+            if (match !== null && match[1] === qualifiedName) {
+                // the import already exists, so there's no need to add an import line
+                return;
+            }
+        }
+
+        if (addExtraNewline && pkgline + 2 < insertLineno) {
+            insertLineno = pkgline + 2;
+        }
+
+        console.log(`pkgline = ${pkgline}, insertLineno = ${insertLineno}`);
+
+        const edit = new vscode.WorkspaceEdit();
+        edit.insert(
+            document.uri,
+            document.lineAt(insertLineno).range.start,
+            `import ${qualifiedName}\n${addExtraNewline ? '\n' : ''}`,
+        );
+        vscode.workspace.applyEdit(edit);
+    }));
 
     context.subscriptions.push(vscode.workspace.onDidCreateFiles(event => {
         for (const file of event.files) {
             const unixpath = file.path;
             if (unixpath.endsWith(".cj")) {
-                const triple = model.parseUnixPath(unixpath);
+                const triple = model.parseSourceUri(file);
                 if (triple !== null) {
                     const [, pkg, clsname] = triple;
                     fs.writeFile(file, Buffer.from(`package ${pkg}
@@ -27,12 +101,104 @@ class ${clsname} {
         if (document.languageId !== 'cj') {
             return undefined;
         }
+        lazyInit(document.uri);
         console.log(`document.languageId = ${document.languageId}, document.fileName = ${document.fileName}`);
-    }))
+    }));
 
     context.subscriptions.push(vscode.workspace.onDidCloseTextDocument(document => {
         if (document.languageId !== 'cj') {
             return undefined;
         }
     }));
+
+    context.subscriptions.push(vscode.languages.registerCompletionItemProvider('cj', {
+        provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+            try {
+                lazyInit(document.uri);
+
+                const range = document.getWordRangeAtPosition(position);
+                const prefix = document.getText(range);
+                if (prefix.length === 0) {
+                    return undefined;
+                }
+
+                let pkg = "";
+                let clsname = "";
+                {
+                    const sourceTriple = model.parseSourceUri(document.uri);
+                    if (sourceTriple !== null) {
+                        const [, pkg_, clsname_] = sourceTriple;
+                        pkg = pkg_;
+                        clsname = clsname_;
+                    }
+                }
+
+                // completion based on class names
+                console.log(`prefix = ${prefix}`);
+                addSourceRoot(document.uri);
+                const items = Array.from(world.shortNameToQualifiedNames.filterWithPrefix(prefix)).flatMap((pair) => {
+                    const [shortName, qualifiedNames] = pair;
+                    return Array.from(qualifiedNames).sort().map(qualifiedName => {
+                        const item = new vscode.CompletionItem(shortName);
+                        item.detail = qualifiedName;
+                        item.command = {
+                            command: COMMAND_AUTO_IMPORT,
+                            title: 'autoimport',
+                            arguments: [qualifiedName],
+                        };
+                        return item;
+                    });
+                });
+
+                // completion based on local names
+                {
+                    const item = world.getItemOrNull(pkg + '.' + clsname);
+                    if (item !== null) {
+                        for (const name of item.localNames) {
+                            items.push(new vscode.CompletionItem(name));
+                        }
+                    }
+                }
+
+                console.log(`items.length = ${items.length}`)
+
+                return items;
+            } catch (e) {
+                console.log(e.stack);
+                console.log('ERROR: ' + e);
+            }
+        }
+    }));
+
+    context.subscriptions.push(vscode.languages.registerCompletionItemProvider('cj', {
+        provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+            try {
+                lazyInit(document.uri);
+                const range = document.getWordRangeAtPosition(position);
+
+                const items: vscode.CompletionItem[] = [];
+
+                // completion based on method and field names
+                const prefix = range === undefined ? "" : document.getText(range);
+                const linePrefix = document.lineAt(position).text.substr(0, position.character);
+                if (linePrefix.endsWith('.' + prefix)) {
+                    for (const fieldName of world.allFieldNames) {
+                        if (fieldName.startsWith(prefix)) {
+                            items.push(new vscode.CompletionItem(fieldName));
+                        }
+                    }
+                    for (const methodName of world.allMethodNames) {
+                        if (methodName.startsWith(prefix)) {
+                            items.push(new vscode.CompletionItem(methodName + '('));
+                        }
+                    }
+                }
+                return items;
+            } catch (e) {
+                console.log(e.stack);
+                console.log('' + e);
+                throw e;
+            }
+        }
+    }, '.'));
 }
--- ide/language-cj/src/model.ts
+++ ide/language-cj/src/model.ts
@@ -1,6 +1,58 @@
+import * as vscode from 'vscode';
+
+export const IMPORT_EXEMPT_CLASSES = new Set([
+    "cj.Unit",
+    "cj.NoReturn",
+    "cj.Bool",
+    "cj.Int",
+    "cj.Double",
+    "cj.Char",
+    "cj.String",
+    "cj.Buffer",
+    "cj.MutableList",
+    "cj.MutableMap",
+    "cj.MutableSet",
+    "cj.Collection",
+    "cj.Clone",
+    "cj.List",
+    "cj.Map",
+    "cj.Set",
+    "cj.Fn0",
+    "cj.Fn1",
+    "cj.Fn2",
+    "cj.Fn3",
+    "cj.Fn4",
+    "cj.Tuple2",
+    "cj.Tuple3",
+    "cj.Tuple4",
+    "cj.Try",
+    "cj.Error",
+    "cj.Nullable",
+    "cj.IO",
+    "cj.AIO",
+    "cj.FS",
+    "cj.Iterable",
+    "cj.Iterator",
+    "cj.ToBool",
+    "cj.ToInt",
+    "cj.ToDouble",
+    "cj.ToChar",
+    "cj.ToString",
+    "cj.ToList",
+    "cj.ListBuilder",
+    "cj.MapBuilder",
+    "cj.SetBuilder",
+    "cj.Promise",
+    "cj.Math",
+    "cj.Repr",
+    "cj.Eq",
+    "cj.Ord",
+    "cj.Hash",
+    "cj.Assert",
+]);
 
 export class OpenDocument {
-    words: Set<String>
+    words: Set<string>
 
     constructor() {
         this.words = new Set();
@@ -15,7 +67,7 @@ export class OpenDocument {
  *
  * If the path is invalid, returns null
  */
-export function parseUnixPath(unixpath: string) : [string, string, string] | null {
+export function parseUnixPath(unixpath: string): [string, string, string] | null {
 
     if (!unixpath.endsWith('.cj')) {
         return null;
@@ -37,4 +89,401 @@ export function parseUnixPath(unixpath: string) : [string, string, string] | nul
     }
 
     return null;
+}
+
+/**
+ * Parses a file system path and returns the triple:
+ *  - source root path (i.e. path ending in '/src/main/cj/' or '/src/main/cj-js/')
+ *  - package name (e.g. 'cjx.foobar')
+ *  - class name (e.g. Nullable)
+ *
+ * If the path is invalid, returns null
+ */
+export function parseSourceUri(uri: vscode.Uri): [string, string, string] | null {
+    return parseUnixPath(uri.path);
+}
+
+
+class Trie<T> {
+    root: TrieNode<T>
+    constructor() {
+        this.root = new TrieNode();
+    }
+    has(key: string): boolean {
+        return this.get(key) !== null;
+    }
+    get(key: string): T | null {
+        return this.root.get(key, 0);
+    }
+    set(key: string, value: T): boolean {
+        return !!this.root.set(key, 0, value);
+    }
+    remove(key: string): T | null {
+        return this.root.remove(key, 0);
+    }
+    [Symbol.iterator]() {
+        return this.root[Symbol.iterator]();
+    }
+    filterWithPrefix(prefix: string): IterableIterator<T> {
+        let node: TrieNode<T> = this.root;
+        for (let i = 0; i < prefix.length; i++) {
+            const childNode = node.map.get(prefix[i]);
+            if (childNode === undefined) {
+                return [][Symbol.iterator]();
+            }
+            node = childNode;
+        }
+        return node[Symbol.iterator]();
+    }
+}
+
+
+class TrieNode<T> {
+    size: number
+    value: T | null
+    map: Map<string, TrieNode<T>>
+
+    constructor() {
+        this.size = 0;
+        this.value = null;
+        this.map = new Map();
+    }
+
+    get(key: string, index: number): T | null {
+        if (index === key.length) {
+            return this.value;
+        } else if (index < key.length) {
+            const child = this.map.get(key[index]);
+            if (child === undefined) {
+                return null;
+            } else {
+                return child.get(key, index + 1);
+            }
+        } else {
+            return null;
+        }
+    }
+
+    set(key: string, index: number, value: T): boolean {
+        if (index === key.length) {
+            if (this.value !== null) {
+                return false;
+            } else {
+                this.value = value;
+                this.size += 1;
+                return true;
+            }
+        } else if (index < key.length) {
+            const ch = key[index];
+            if (!this.map.has(ch)) {
+                this.map.set(ch, new TrieNode());
+            }
+            const incr = this.map.get(ch)!.set(key, index + 1, value);
+            this.size += incr ? 1 : 0;
+            return incr;
+        } else {
+            return false;
+        }
+    }
+
+    remove(key: string, index: number): T | null {
+        if (index === key.length) {
+            if (this.value != null) {
+                const ret = this.value;
+                this.value = null;
+                this.size -= 1;
+                return ret;
+            } else {
+                return null;
+            }
+        } else if (index < key.length) {
+            const ch = key[index];
+            const child = this.map.get(ch);
+            if (child === undefined) {
+                return null;
+            }
+            const ret = child.remove(key, index + 1);
+            if (child.size === 0) {
+                this.map.delete(ch);
+            }
+            this.size -= 1;
+            return ret;
+        } else {
+            return null;
+        }
+    }
+
+    *[Symbol.iterator](): IterableIterator<T> {
+        if (this.value !== null) {
+            yield this.value;
+        }
+        for (const child of Array.from(this.map).sort().map(pair => pair[1])) {
+            for (const item of child) {
+                yield item;
+            }
+        }
+    }
+}
+
+/**
+ * Set with repeats allowed
+ */
+class RCSet<T> {
+    map: Map<T, number>
+
+    constructor() {
+        this.map = new Map();
+    }
+
+    has(key: T): boolean {
+        return this.map.has(key);
+    }
+
+    add(key: T) {
+        const rc = this.map.get(key);
+        if (rc !== undefined) {
+            this.map.set(key, rc + 1);
+        } else {
+            this.map.set(key, 1);
+        }
+    }
+
+    addAll(keys: Iterable<T>) {
+        for (const key of keys) {
+            this.add(key);
+        }
+    }
+
+    remove(key: T) {
+        const rc = this.map.get(key);
+        if (rc !== undefined) {
+            if (rc === 1) {
+                this.map.delete(key);
+            } else {
+                this.map.set(key, rc - 1);
+            }
+        }
+    }
+
+    removeAll(keys: Iterable<T>) {
+        for (const key of keys) {
+            this.remove(key);
+        }
+    }
+
+    [Symbol.iterator]() {
+        return this.map.keys();
+    }
+}
+
+function parseItem(s: string): Item {
+    const re = /\\{|\\}|\b(def|val|var|class|union|trait)\s+\w+|\b(import|package)\s+[\.\w]+|\w+(?:\s*:)?|"(?:[^"]|\")*"|#[^\n]*|[^\s\w"]+/g;
+    let depth = 0;
+    let arr;
+    let pkg = "";
+    let shortName = "";
+    const imports = new Trie<string>();
+    const methodNames = new Trie<string>();
+    const fieldNames = new Trie<string>();
+    const localNames = new Trie<string>();
+    while ((arr = re.exec(s)) !== null) {
+        const x = arr[0];
+        switch (x) {
+            case '{':
+                depth++;
+                break;
+            case '}':
+                depth--;
+                break;
+            default: {
+                const parts = /\b(def|val|var|class|union|trait|import|package|)\s+([\w\.]+)\s*:?/.exec(x);
+                if (parts !== null) {
+                    const [, kind, name] = parts;
+                    switch (kind) {
+                        case 'package':
+                            if (pkg === "") {
+                                pkg = name;
+                            }
+                            break;
+                        case 'import':
+                            imports.set(name, name);
+                            break;
+                        case 'def':
+                            methodNames.set(name, name);
+                            break;
+                        case 'val':
+                        case 'var':
+                            if (depth === 1) {
+                                fieldNames.set(name, name);
+                            } else {
+                                localNames.set(name, name);
+                            }
+                            break;
+                        case 'class':
+                        case 'union':
+                        case 'trait':
+                            if (shortName === "") {
+                                shortName = name;
+                            }
+                            break;
+                        default:
+                            if (/[a-z_]\w*/.test(name)) {
+                                localNames.set(name, name);
+                            }
+                            break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+    return new Item(pkg, shortName, imports, methodNames, fieldNames, localNames);
+}
+
+export class Item {
+    readonly pkg: string
+    readonly shortName: string
+    readonly imports: Trie<string>
+    readonly methodNames: Trie<string>
+    readonly fieldNames: Trie<string>
+    readonly localNames: Trie<string>
+
+    constructor(
+            pkg: string,
+            shortName: string,
+            imports: Trie<string>,
+            methodNames: Trie<string>,
+            fieldNames: Trie<string>,
+            localNames: Trie<string>) {
+        this.pkg = pkg;
+        this.shortName = shortName;
+        this.imports = imports;
+        this.methodNames = methodNames;
+        this.fieldNames = fieldNames;
+        this.localNames = localNames;
+    }
+
+    toString() {
+        return (
+            `Item("${this.pkg}", "${this.shortName}", ` +
+            `"[${Array.from(this.imports)}]", ` +
+            `[${Array.from(this.methodNames)}], ` +
+            `[${Array.from(this.fieldNames)}])`
+        );
+    }
+}
+
+export class World {
+    readonly fs: vscode.FileSystem
+    readonly sourceRoots: Set<string>
+    readonly shortNameToQualifiedNames: Trie<[string, Set<string>]>
+    readonly qualifiedNameToUri: Map<string, vscode.Uri>
+    readonly qualifiedNameToItem: Trie<Item>
+    readonly allMethodNames: RCSet<string>
+    readonly allFieldNames: RCSet<string>
+    _allKnownItemsInitialized: boolean
+
+    constructor(fs: vscode.FileSystem) {
+        this.fs = fs;
+        this.sourceRoots = new Set();
+        this.shortNameToQualifiedNames = new Trie();
+        this.qualifiedNameToUri = new Map();
+        this.qualifiedNameToItem = new Trie();
+        this.allMethodNames = new RCSet();
+        this.allFieldNames = new RCSet();
+        this._allKnownItemsInitialized = false;
+    }
+
+    async addSourceRoot(sourceRoot: string): Promise<void> {
+        if (!this.sourceRoots.has(sourceRoot)) {
+            this.sourceRoots.add(sourceRoot);
+            await this._processSourceRoot(sourceRoot);
+        }
+    }
+
+    async _processSourceRoot(sourceRoot: string) {
+        this.sourceRoots.add(sourceRoot);
+        const stack = [vscode.Uri.file(sourceRoot)];
+        const otherPromises: Promise<Item>[] = [];
+        while (stack.length > 0) {
+            const uri = stack.pop()!;
+            for (const [name, type] of await this.fs.readDirectory(uri)) {
+                const child = vscode.Uri.joinPath(uri, name);
+                if (type === vscode.FileType.Directory) {
+                    stack.push(child);
+                } else if (type === vscode.FileType.File && name.endsWith(".cj")) {
+                    const triple = parseSourceUri(child);
+                    if (triple !== null) {
+                        const [, pkg, shortName] = triple;
+                        const qualifiedName = pkg + '.' + shortName;
+                        console.log(`FFF ${qualifiedName}`)
+
+                        let pair = this.shortNameToQualifiedNames.get(shortName);
+                        if (pair === null) {
+                            pair = [shortName, new Set()];
+                            this.shortNameToQualifiedNames.set(shortName, pair);
+                        }
+                        pair[1].add(qualifiedName);
+
+                        this.qualifiedNameToUri.set(qualifiedName, child);
+                        otherPromises.push(this.refreshItem(qualifiedName));
+                    }
+                }
+            }
+        }
+
+        for (const promise of otherPromises) {
+            await promise;
+        }
+    }
+
+    getItemOrNull(qualifiedName: string): Item | null {
+        const item = this.qualifiedNameToItem.get(qualifiedName);
+        if (item === null) {
+            this.getItem(qualifiedName).catch(e => {
+                console.log(`#### getItem failed ${e}`);
+            });
+            return null;
+        } else {
+            return item;
+        }
+    }
+
+    async getItem(qualifiedName: string): Promise<Item> {
+        const item = this.qualifiedNameToItem.get(qualifiedName);
+        if (item) {
+            return item;
+        } else {
+            return await this.refreshItem(qualifiedName);
+        }
+    }
+
+    async refreshItem(qualifiedName: string): Promise<Item> {
+        // If there's nothing there, put in a dummy value to prevent spurious refreshes.
+        if (!this.qualifiedNameToItem.has(qualifiedName)) {
+            this.qualifiedNameToItem.set(qualifiedName, parseItem(""));
+        }
+        const newItem = await this._getItemUncached(qualifiedName);
+        const oldItem = this.qualifiedNameToItem.get(qualifiedName);
+        if (oldItem) {
+            // "release" item members
+            this.allFieldNames.removeAll(oldItem.fieldNames);
+            this.allMethodNames.removeAll(oldItem.methodNames);
+        }
+        // "retain" item members
+        this.allFieldNames.addAll(newItem.fieldNames);
+        this.allMethodNames.addAll(newItem.methodNames);
+        this.qualifiedNameToItem.set(qualifiedName, newItem);
+        return newItem;
+    }
+
+    async _getItemUncached(qualifiedName: string): Promise<Item> {
+        const uri = this.qualifiedNameToUri.get(qualifiedName);
+        if (uri === undefined) {
+            console.log(`URI for ${qualifiedName} not found => ${Array.from(this.qualifiedNameToUri.keys())}`);
+            return parseItem("");
+        }
+        const buffer = Buffer.from(await this.fs.readFile(uri));
+        return parseItem(buffer.toString('utf-8'));
+    }
 }
